80527502e97e14fb74bff54249c156c36e88ded9
angulo alejo <angulojoaquin771gmail.com>
2022-11-13 21:21:31 -0300

    se agrega un let

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,4 @@ const inputUsuario = document.getElementById('usuario-correo');
 const inputContraseña = document.getElementById('contraseña');
 const perro1 = document.getElementById('perro-serio');
 const body = document.querySelector('body');
+let carafeliz = true;
