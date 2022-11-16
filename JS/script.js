6408862ab00c7a75c1c043baeeaef8f9f59c522a
angulo alejo <angulojoaquin771gmail.com>
2022-11-16 13:41:02 -0300

    se hacen cambios en los archivos

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,18 @@
 const inputUsuario = document.getElementById('usuario-correo');
 const inputContraseña = document.getElementById('contraseña');
-const perro1 = document.getElementById('perro-serio');
+const perro1 = document.getElementById('perro');
 const body = document.querySelector('body');
 let carafeliz = true;
+
+inputContraseña.addEventListener('focus',()=>{
+    carafeliz = false;
+    let cont = 1;
+    const caraenojao = setInterval(() => {
+        monster.src = 'img/'+cont+'.png';
+        if(cont < 10){
+            cont++;
+        }else{
+            clearInterval(caraenojao);
+        }
+    }, 60);
+})
